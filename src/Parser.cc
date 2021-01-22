377d42df169615a6e7896d099d9e53a1b7c09405
adamnapieralski <adam.napieralski@gmail.com>
2021-01-22 10:02:15 +0100

    add readStringData

--- src/Parser.cc
+++ src/Parser.cc
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <string>
 
 #include "utils.hpp"
 
@@ -42,7 +43,63 @@ SequenceMap parser::readCharDataNoSep(std::ifstream &f, char sep) {
   }
 
   std::cout << "Char to Int map: " << utils::printMap(char_to_int) << std::endl;
+  return seqs;
+}
 
+SequenceMap parser::readStringData(std::ifstream &f, std::string idSep, std::string elemSep) {
+  int seq_id = 0;
+  int time_id = 0;
+  int sepPos = 0;
+
+  SequenceMap seqs;
+
+  std::map<std::string, int> string_to_int;
+  auto max_int = 0;
+
+  std::string line;
+  while (!f.eof()) {
+    getline(f, line);
+    if (line.length() == 0) break;
+
+    // read sid
+    sepPos = line.find(idSep);
+    std::cout << line.substr(0, sepPos) << std::endl;
+    seq_id = std::stoi(line.substr(0, sepPos));
+    line.erase(0, sepPos + idSep.length());
+
+    // read tid
+    sepPos = line.find(idSep);
+    std::cout << line.substr(0, sepPos) << std::endl;
+    time_id = std::stoi(line.substr(0, sepPos));
+    line.erase(0, sepPos + idSep.length());
+
+    auto &seq_item = seqs[seq_id];
+
+    while (line.size() > 0) {
+      sepPos = line.find(elemSep);
+      std::string elem = "";
+      if (sepPos != std::string::npos) {
+        elem = line.substr(0, sepPos);
+        line.erase(0, sepPos + elemSep.length());
+      } else {
+        // only \n, may add \r for Windows
+        line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
+        elem = line;
+        line = "";
+      }
+      auto it = string_to_int.find(elem);
+      if (it == string_to_int.end()) {
+        string_to_int[elem] = ++max_int;
+        seq_item.push_back(max_int);
+      } else {
+        seq_item.push_back(it->second);
+      }
+    }
+
+    seq_item.push_back(-1);
+  }
+
+  std::cout << "String to Int map: " << utils::printMap(string_to_int) << std::endl;
   return seqs;
 }
 
--- src/Parser.h
+++ src/Parser.h
@@ -4,12 +4,14 @@
 #include <fstream>
 #include <unordered_map>
 #include <vector>
+#include <string>
 
 using SequenceMap = std::unordered_map<int, std::vector<int>>;
 
 namespace parser {
 
 SequenceMap readCharDataNoSep(std::ifstream& f, char sep);
+SequenceMap readStringData(std::ifstream &f, std::string idSep, std::string elemSep);
 SequenceMap readSpfm(std::ifstream& f, int limit);
 
 }  // namespace parser
